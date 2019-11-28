9e7b57c6fd730a38c85976c07137b1efe8916e44
lingulist <mattis.list@lingpy.org>
2019-11-28 13:42:38 +0100

    update orthos

--- lexibank_chindialectsurvey.py
+++ lexibank_chindialectsurvey.py
@@ -1,23 +1,12 @@
 from pathlib import Path
 
-<<<<<<< HEAD
 from pylexibank.dataset import Dataset as BaseDataset
 from pylexibank import progressbar
-=======
 import attr
->>>>>>> 2303188b6aead7706356226e2d50b999c807f8ce
 from clldutils.misc import slug
 from pylexibank import Concept, Language, FormSpec
 from pylexibank.dataset import Dataset as BaseDataset
 from pylexibank.util import progressbar
-
-<<<<<<< HEAD
-import attr
-from pylexibank import Concept, Language
-from pylexibank.forms import FormSpec
-
-=======
->>>>>>> 2303188b6aead7706356226e2d50b999c807f8ce
 
 @attr.s
 class CustomConcept(Concept):
@@ -45,45 +34,23 @@ class Dataset(BaseDataset):
 
     # define the way in which forms should be handled
     form_spec = FormSpec(
-<<<<<<< HEAD
-            brackets={"(": ")", '[': ']'},
-            separators = ";/,",
-            missing_data = ('?', '-', '0'),
-            strip_inside_brackets=True
-            )
-=======
         brackets={"(": ")", "[": "]"},
         separators=";/,",
         missing_data=("?", "-"),
         strip_inside_brackets=True,
     )
->>>>>>> 2303188b6aead7706356226e2d50b999c807f8ce
 
     def cmd_makecldf(self, args):
         """
         Convert the raw data to a CLDF dataset.
         """
-<<<<<<< HEAD
         data = self.raw_dir.read_csv('wordlist.tsv', dicts=True, delimiter='\t')
         args.writer.add_sources()
         languages = args.writer.add_languages(lookup_factory="ID")
         concepts = {}
-=======
-        data = self.raw_dir.read_csv("wordlist.tsv", dicts=True, delimiter="\t")
-        concept_lookup = {}
-        args.writer.add_sources()
-
-        language_lookup = set()
-        for language in self.languages:
-            if language["KEEP"] == "1":
-                args.writer.add_language(**{k: v for (k, v) in language.items() if k != "KEEP"})
-                language_lookup.add(language["ID"])
-
->>>>>>> 2303188b6aead7706356226e2d50b999c807f8ce
         for concept in self.concepts:
             idx = concept["ID"].split("-")[-1] + "_" + slug(concept["ENGLISH"])
             args.writer.add_concept(
-<<<<<<< HEAD
                     ID=idx,
                     Name=concept['ENGLISH'],
                     AlternativeName=concept['AlternativeName'],
@@ -91,33 +58,12 @@ class Dataset(BaseDataset):
                     Concepticon_Gloss=concept['CONCEPTICON_GLOSS'],
                     )
             concepts[concept['ENGLISH']] = idx
-        for row in progressbar(data, desc='cldfify'):
-            if row['DOCULECT'] in languages:
-                args.writer.add_forms_from_value(
-                        Language_ID=row['DOCULECT'],
-                        Parameter_ID=concepts[row['CONCEPT']],
-                        Value=row['TRANSCRIPTION'],
-                        Source=['chinds']
-                        )
-
-
-
-                    
-=======
-                ID=idx,
-                Name=concept["ENGLISH"],
-                AlternativeName=concept["AlternativeName"],
-                Concepticon_ID=concept["CONCEPTICON_ID"],
-                Concepticon_Gloss=concept["CONCEPTICON_GLOSS"],
-            )
-            concept_lookup[concept["ENGLISH"]] = idx
 
         for row in progressbar(data, desc="cldfify"):
-            if row["DOCULECT"] in language_lookup:
+            if row["DOCULECT"] in languages:
                 args.writer.add_forms_from_value(
                     Language_ID=row["DOCULECT"],
-                    Parameter_ID=concept_lookup[row["CONCEPT"]],
+                    Parameter_ID=concepts[row["CONCEPT"]],
                     Value=row["TRANSCRIPTION"],
                     Source=["chinds"],
                 )
->>>>>>> 2303188b6aead7706356226e2d50b999c807f8ce
